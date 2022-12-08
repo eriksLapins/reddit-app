b8ea776aafcd3c789d29003439aaa8dc3daed5ad
Ēriks Lapiņš <lapinseriks98@gmail.com>
2022-12-08 23:25:26 +0200

    Started working on the navbar

---
+++ src/components/navBar/Navbar.js
@@ -0,0 +1,22 @@
+import { NavLink, Link, useMatch, useResolvedPath } from "react-router-dom"
+
+export default function NavBar() {
+    return <nav className="nav">
+        <a href="/" className="site-title">Site name</a>
+        <ul>
+            <CustomLink to="/posts">Posts</CustomLink>
+            <CustomLink to="/search">Search</CustomLink>
+        </ul>
+    </nav>
+}
+
+function CustomLink({to, children, ...props}) {
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true})
+    return (
+        <NavLink className={isActive ? "active" : ""}>
+            <Link to={to} {...props}>{children}</Link>
+        </NavLink>
+        )
+
+}
