a1cc0bdb74cef6c94d620ceff73983c8851307f2
Ēriks Lapiņš <lapinseriks98@gmail.com>
2022-12-09 20:21:06 +0200

    made h1 to be in the middle, fixed activeclass

--- src/components/navBar/Navbar.js
+++ src/components/navBar/Navbar.js
@@ -1,22 +1,22 @@
 import { NavLink, Link, useMatch, useResolvedPath } from "react-router-dom"
 
 export default function NavBar() {
-    return <nav className="nav">
+    return (<nav className="nav">
         <a href="/" className="site-title">Site name</a>
         <ul>
             <CustomLink to="/posts">Posts</CustomLink>
             <CustomLink to="/search">Search</CustomLink>
         </ul>
-    </nav>
+    </nav>)
 }
 
 function CustomLink({to, children, ...props}) {
     const resolvedPath = useResolvedPath(to)
     const isActive = useMatch({ path: resolvedPath.pathname, end: true})
     return (
-        <NavLink className={isActive ? "active" : ""}>
+        <li className={isActive ? "active" : ""}>
             <Link to={to} {...props}>{children}</Link>
-        </NavLink>
+        </li>
         )
 
 }
