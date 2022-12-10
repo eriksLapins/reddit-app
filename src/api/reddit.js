da8dba59e1c3d0a533e76870132c877a9762a1dc
Ēriks Lapiņš <lapinseriks98@gmail.com>
2022-12-10 19:08:47 +0200

    added redditSlice and subRedditSlice

--- src/api/reddit.js
+++ src/api/reddit.js
@@ -7,6 +7,13 @@ export const getSubredditPosts = async (subreddit) => {
   return json.data.children.map((post) => post.data);
 };
 
+export const getSubreddits = async () => {
+  const response = await fetch(`${API_ROOT}/subreddits.json`);
+  const json = await response.json()
+
+  return json.data.children.map((subreddit) => subreddit.data);
+}
+
 export const getPostComments = async (permalink) => {
   const response = await fetch(`${API_ROOT}${permalink}.json`);
   const json = await response.json();
